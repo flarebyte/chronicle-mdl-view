28c819003593fcc852350138685428f7166f0662
olih <olih@users.noreply.github.com>
2017-04-15 11:50:25 +0100

    basic skeleton

---
+++ README.md
@@ -0,0 +1,8 @@
+# A material design view in Elm for editing rdf content
+
+[![Build Status](https://travis-ci.org/flarebyte/chronicle-mdl-view.svg?branch=master)](https://travis-ci.org/flarebyte/chronicle-mdl-view)
+
+Experimental package for a customizable rdf editor  
+Note that this API is experimental and likely to go through many more iterations.
+
+Feedback and contributions are very welcome.
---
+++ tests/Tests.elm
@@ -0,0 +1,17 @@
+module Tests exposing (..)
+
+import Test exposing (..)
+import Expect
+import Fuzz exposing (list, int, tuple3)
+import Chronicle.View exposing (..)
+
+
+all : Test
+all =
+    describe "Chronicle.View"
+        [ describe "unique" <|
+            [ test "removes duplicates" <|
+                \() ->
+                    Expect.equal (Chronicle.View.unique [ 0, 1, 1, 0, 1 ]) [ 0, 1 ]
+            ]
+        ]
