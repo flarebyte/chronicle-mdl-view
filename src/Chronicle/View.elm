4711eb86633a733c15ec85a38cb804137ad5abcc
olih <olih@users.noreply.github.com>
2017-04-17 10:26:58 +0100

    Working drawer

--- src/Chronicle/View.elm
+++ src/Chronicle/View.elm
@@ -13,12 +13,15 @@ module Chronicle.View
 
 import List exposing (..)
 import Html exposing (..)
-import Html.Attributes exposing (href, class, style)
+import Html.Attributes exposing (href, class, style, src)
 import Material
 import Material.Scheme
 import Material.Button as Button
 import Material.Options as Options exposing (css)
 import Material.Layout as Layout
+import Material.Color as Color
+import Material.Menu as Menu
+import Material.Options as Options exposing (css, cs, when)
 
 
 
@@ -89,82 +92,52 @@ update msg model =
 type alias Mdl =
     Material.Model
 
-appHeader =
-  header [ class "mdl-layout__header" ]
-    [ div [ class "mdl-layout__header-row" ]
-        [ span [ class "mdl-layout-title" ]
-            [ text "Title" ]
-        , div [ class "mdl-layout-spacer" ]
+viewHeader : Model -> Html Msg
+viewHeader model =
+    Layout.row
+        []
+        [ Layout.title [] [ text "elm-mdl Dashboard Example" ]
+        , Layout.spacer
+        , Layout.navigation []
             []
-        , nav [ class "mdl-navigation mdl-layout--large-screen-only" ]
-            [ a [ class "mdl-navigation__link", href "" ]
-                [ text "Link" ]
-            , a [ class "mdl-navigation__link", href "" ]
-                [ text "Link" ]
-            , a [ class "mdl-navigation__link", href "" ]
-                [ text "Link" ]
-            , a [ class "mdl-navigation__link", href "" ]
-                [ text "Link" ]
-            ]
         ]
-    ]
 
 navLink = "http://localhost:8000/nav"
 
-appDrawer =
-  div [ class "mdl-layout__drawer" ]
-    [ span [ class "mdl-layout-title" ]
-        [ text "Title" ]
-    , nav [ class "mdl-navigation" ]
-        [ a [ class "mdl-navigation__link", href navLink ]
-            [ text "Link" ]
-        , a [ class "mdl-navigation__link", href navLink ]
-            [ text "Link" ]
-        , a [ class "mdl-navigation__link", href navLink ]
-            [ text "Link" ]
-        , a [ class "mdl-navigation__link", href navLink ]
-            [ text "Link" ]
+
+drawerHeader : Model -> Html Msg
+drawerHeader model =
+    Layout.navigation []
+    [
+    Button.render Mdl
+        [ 0 ]
+        model.mdl
+        [ Options.onClick Increase
+        , css "margin" "0 24px"
         ]
+        [ text "Increase" ]
+    , Button.render Mdl
+        [ 1 ]
+        model.mdl
+        [ Options.onClick Reset ]
+        [ text "Reset" ]
     ]
+
+
+
 view : Model -> Html Msg
 view model =
     div
         [ style [ ( "padding", "2rem" ) ] ]
-        [ text ("Current count: " ++ toString model.count)
-          {- We construct the instances of the Button component that we need, one
-             for the increase button, one for the reset button. First, the increase
-             button. The first three arguments are:
-               - A Msg constructor (`Mdl`), lifting Mdl messages to the Msg type.
-               - An instance id (the `[0]`). Every component that uses the same model
-                 collection (model.mdl in this file) must have a distinct instance id.
-               - A reference to the elm-mdl model collection (`model.mdl`).
-             Notice that we do not have to add fields for the increase and reset buttons
-             separately to our model; and we did not have to add to our update messages
-             to handle their internal events.
-             Mdl components are configured with `Options`, similar to `Html.Attributes`.
-             The `Options.onClick Increase` option instructs the button to send the `Increase`
-             message when clicked. The `css ...` option adds CSS styling to the button.
-             See `Material.Options` for details on options.
-          -}
-        , Layout.render Mdl model.mdl
+        [ Layout.render Mdl model.mdl
         [ Layout.fixedHeader ]
-        { header = [ appHeader ]
-        , drawer = [appDrawer]
+        { header = [ viewHeader model ]
+        , drawer = [ drawerHeader model ]
         , tabs = ([], [])
-        , main = []
+        , main = [
+          text ("Current count: " ++ toString model.count)
+        ]
         }
-        , Button.render Mdl
-            [ 0 ]
-            model.mdl
-            [ Options.onClick Increase
-            , css "margin" "0 24px"
-            ]
-            [ text "Increase" ]
-        , Button.render Mdl
-            [ 1 ]
-            model.mdl
-            [ Options.onClick Reset ]
-            [ text "Reset" ]
         ]
         |> Material.Scheme.top
 
